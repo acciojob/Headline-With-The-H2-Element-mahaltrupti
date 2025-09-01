79f51bee02ef5c63ad082fdfa83020559fda6de4
mahaltrupti <truptimahal2004@gmail.com>
2025-09-01 22:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Your script here.
+  <script>
+    // JavaScript Function
+    function changeHeading() {
+      document.getElementById("subheading").innerText = "Welcome to Acciojob!";
+    }
+  </script>
